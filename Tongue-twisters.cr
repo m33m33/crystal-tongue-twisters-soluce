d0ac959c7c1a52e9cc91b4fcf1f72f2d7cc1d72b
M33 <327-m33@git.qoto.org>
2021-04-02 13:34:43 +0000

    Add new file

---
+++ Tongue-twisters.cr
@@ -0,0 +1,9 @@
+w=" woodchuck "
+p=" pickled "
+m="peppers"
+l="Peter Piper picked"
+s=" seashells"
+x=" seashore"
+y="he sells"
+c=" chuck wood"
+print("How much wood would a"+w+"chuck,\nIf a"+w+"could"+c+"?\nA"+w+"would chuck all the wood he could chuck\nIf a"+w+"would"+c+".\n\n",l+" a peck of"+p+m+".\nA peck of"+p+m+" "+l+".\nIf "+l+" a peck of"+p+m+",\nWhere's the peck of"+p+m+" "+l+"?\n\nS"+y+s+" by the"+x+",\nThe shells s"+y+" are"+s+", I'm sure.\nSo if s"+y+s+" on the"+x+",\nThen I'm sure s"+y+" seashore shells.")
